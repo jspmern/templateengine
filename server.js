ddf34366212407d7141c1a9dce32affaf8029db2
jspmern <jspmern@gmail.com>
2024-02-28 13:15:12 +0530

    template engine (ejs)

---
+++ server.js
@@ -0,0 +1,51 @@
+// let express = require("express");
+// let app = express();
+// let path = require("path");
+// let fs = require("fs");
+// app.use(express.static("public"));
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public/index.html"));
+// });
+// app.get("/about", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public/About.html"));
+// });
+// app.get("/contact", (req, res) => {
+//   let x = { login: true, name: "utsav" };
+//   let temp = path.join(__dirname, "public/Contact.html");
+//   fs.readFile(temp, "utf-8", (err, data) => {
+//     if (err) {
+//       res.send("somthing wrong");
+//     } else {
+//       let newData = data.replace("<!-- dynamic -->", x.name);
+//       res.end(newData);
+//     }
+//   });
+// });
+// app.listen(8080, () => {
+//   console.log("connect");
+// });
+
+
+
+
+
+
+
+
+// /..................ejs..............................
+let express=require('express')
+let app=express()
+app.set('view engine','ejs')
+app.use(express.static('views'))
+ app.get('/',(req,res)=>{
+    res.render('pages/index')
+ })
+ app.get('/about',(req,res)=>{
+    res.render('pages/About')
+ })
+ app.get('/contact',(req,res)=>{
+    res.render('pages/Contact')
+ })
+app.listen(8080,()=>{
+    console.log('connect')
+})
